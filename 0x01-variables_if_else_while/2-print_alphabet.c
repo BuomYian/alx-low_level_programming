a921305ee9ff8bda474bc76a19548a371ab8626e
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-01 07:45:18 -0700

    added betty styl

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
-
 /**
  *main - print lowercase a-z
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char alpha = 'a';
-
 	while (alpha <= 'z')
 	{
 		putchar(alpha);
 		alpha++;
 	}
 	putchar('\n');
-
 	return (0);
 }
