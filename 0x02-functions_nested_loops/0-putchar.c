168b1e96b232c549e2fef06b2b039508880c1534
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-05 04:32:17 -0700

    added main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "alx.h"
+#include "main.h"
 
 /**
   *main - Entry point
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+  *_putchar - writes the character c to std
+  *@c: parameter to compare
+  *Return: 1 or 0
+  */
+int _putchar(char c);
