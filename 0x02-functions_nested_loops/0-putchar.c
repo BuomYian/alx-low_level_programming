962edcd8cce81f49c02a60ee0fa3e0da49fbf97c
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-05 03:53:44 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "alx.h"
+
+/**
+  *main - Entry point
+  *
+  *Return: Always returns 0
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
