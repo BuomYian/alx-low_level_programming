359ea4276c45b38728aba256e2c8bdc31057b500
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-05 13:34:56 -0700

    fixed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -32,7 +32,7 @@ int main(void)
 	{
 		overflow = (a_tail + b_tail) / 1000000000;
 		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
-		sum_head = (a_tail + b_head) + overflow;
+		sum_head = (a_head + b_head) + overflow;
 
 		printf(", %lu%lu", sum_head, sum_tail);
 
