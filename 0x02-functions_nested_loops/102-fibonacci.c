03f35fabf8783002123fd007faec0bb33ebf462b
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-05 12:21:37 -0700

    fixed

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,10 +12,10 @@ int main(void)
 	long int b = a + 1;
 	long int c = a + b;
 
-	printf("%1d, %1d, ", a, b);
+	printf("%ld, %ld, ", a, b);
 	while (counter < 50)
 	{
-		printf("1d", c);
+		printf("ld", c);
 		counter++;
 		a = b;
 		b = c;
