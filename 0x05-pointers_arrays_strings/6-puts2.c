1c6eb425aa95eec15fc5b62ae6e5311acda354c3
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-11 10:34:18 -0700

    print one character out of two characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *puts2 - Prints one char of two of a string.
+  *@str: The string containing characters.
+  */
+void puts2(char *str)
+{
+	int i, len = 0;
+
+	while (str[i++])
+		len++;
+
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,3 +13,5 @@ void _puts(char *str);
 void print_rev(char *s);
 
 void rev_string(char *s);
+
+void puts2(char *str);
