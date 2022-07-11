01d89199b88d12405ae4a22843bb8a7a8037e15c
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-11 10:22:56 -0700

    strng to be reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "2-strlen.c"
+
 /**
   *rev_string - reverse a string value
   *
@@ -7,13 +7,16 @@
   */
 void rev_string(char *s)
 {
-	int i;
-	char t;
+	int i = 0, len = 0;
+	char tmp;
 
-	for (i = 0; i < _strlen(s) / 2; i++)
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= len / 2; i--)
 	{
-		t = s[i];
-		s[i] = s[_strlen(s) - i - 1];
-		s[_strlen(s) - i - 1] = t;
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
 	}
 }
