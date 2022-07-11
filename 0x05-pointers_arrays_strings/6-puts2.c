ee259c34b1f1353959b99d55e596e47f8c60df53
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-11 14:03:34 -0700

    fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,11 @@
   */
 void puts2(char *str)
 {
-	int i;
+	int i, len;
 
-	for (i = 0; i < _strlen(str); i++)
-	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-	}
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
