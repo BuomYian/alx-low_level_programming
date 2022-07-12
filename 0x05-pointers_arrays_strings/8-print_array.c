c33575a54821a084a71c9e8b2684b82ec35c83ab
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-12 05:10:00 -0700

    print n times

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,8 +3,9 @@
 
 /**
   *print_array - prints n elements of an array of integers
-  *@a:array
-  *@n: number of elements
+  *@a:pointer to array
+  *@n:number of elements
+  *Return:void
   */
 void print_array(int *a, int *n)
 {
