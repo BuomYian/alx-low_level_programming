e77fffb32810dbce429648a82854a7e940f9495e
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-12 04:09:20 -0700

    taks 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  *main - generate random valid passwards
+  *Return: 0 (on success)
+  */
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (srand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+	printf("%c", 2772 - sum);
+
+	return (0);
+}
