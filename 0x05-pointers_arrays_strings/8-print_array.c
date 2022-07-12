c2b7333a8f75b22b609da97efa6d676b5dcef7bd
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-12 03:54:39 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,12 @@ void print_array(int *a, int *n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != (n - 1))
-			printf("%d, ", a[i]);
-		else
-			printf("%d", a[i]);
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
 	}
 	printf("\n");
 }
