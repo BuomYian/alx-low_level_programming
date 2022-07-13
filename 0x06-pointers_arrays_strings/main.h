a96a6a8baba9be7ee9040740d3edd97edf11169f
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-13 11:19:38 -0700

    fixed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
@@ -20,4 +26,8 @@ void print_number(int n);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+
 void print_buffer(char *b, int size);
+
+#endif /*MAIN_H*/
