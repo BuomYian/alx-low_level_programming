2e43140dce12270f48adab6c1054909184b27a54
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-14 09:53:41 -0700

    capitalized

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,27 @@
 #include "main.h"
 
 /**
-  *cap_string - function that capitalize first character of a word
-  *@str: string to capitalized
-  *Return: returns the capitalized string
+  *cap_string - capitalized function
+  *
+  *@x: string param
+  *Return: capitalized version of the string
   */
-char *cap_string(char *str)
+char *cap_string(char *x)
 {
-	int index = 0;
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int len = 13;
+	int a = 0, i;
 
-	while (str[++index])
+	while (x[a])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
-			str[index] -= 32;
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+				x[a] = x[a] - 32;
+			i++;
+		}
+		a++;
 	}
-	return (str);
+	return (x);
 }
