ee0e77737512022b76016af8b8744907de41604b
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-18 10:10:38 -0700

    substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,25 +1,30 @@
 #include "main.h"
-/**
- * _strpbrk - finds string
- * @s: string to find stuff
- * @accept: accepting arrays
- *
- * Return: returns new value
- */
-char *_strpbrk(char *s, char *accept)
-{
-	int i;
-	int j;
 
-	for (i = 0; s[i] != '\0'; i++)
+/**
+ * _strstr - first occurrence of the substring needle in the string haystack
+ * @haystack: main str to be examined
+ * @needle: searched in haystack
+ * Return: return 0
+ **/
+
+char  *_strstr(char *haystack, char *needle)
+{
+	char *str1, *str2; /*Declaring variables*/
+
+	while (*haystack != '\0')
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		str1 = haystack; /*values*/
+		str2 = needle;
+
+		/*Star WHILE*/
+		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
 		{
-			if (s[i] == accept[j])
-			{
-				return (s + i);
-			}
+			haystack++;
+			str2++;
 		}
+		if (*str2 == '\0')
+			return (str1);
+		haystack = str1 + 1;
 	}
 	return (0);
 }
