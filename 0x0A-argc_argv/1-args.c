56e8c92d3959c89f88814252c6c47645d823195c
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-22 07:19:20 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char  *argv[])
+{
+	UNUSED(argv);
+	printf("%d\n", argc - 1);
+	return (0);
+}
