4b7f15d6a668cd47b26f12f616fcac30e36382cf
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-22 07:16:22 -0700

    names

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,3 @@
+int _putchar(char c);
+
+int main(int argc, char *argv[]);
