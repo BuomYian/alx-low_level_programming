ce6839f0d009af84bf6b88bbfc3ff9d40ac1fed2
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-26 07:26:02 -0700

    string concatenate

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - concats strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: returns concated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, len1, len2;
+	char *conc;
+
+	i = 0;
+	len1 = 0;
+	len2 = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i] != '\0')
+	{
+		i++;
+		len1++;
+	}
+
+	i = 0;
+
+	while (s2[i] != '\0')
+	{
+		i++;
+		len2++;
+	}
+
+	conc = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (conc == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+	{
+		conc[i] = s1[i];
+	}
+
+	for (i = 0; i < len2; i++)
+	{
+		conc[i + len1] = s2[i];
+	}
+	conc[i + len1] = '\0';
+	return (conc);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,5 +2,5 @@
 #define MAIN_H
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-
+char *str_concat(char *s1, char *s2);
 #endif
