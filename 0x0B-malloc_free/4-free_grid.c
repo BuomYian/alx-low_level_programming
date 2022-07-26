9dc37868a1870fa227eedb4cd2548f197c28e11f
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-26 07:32:21 -0700

    free up a grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid  - frees up a grid
+ * @grid: grid to check
+ * @height: height of grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (height <= 0)
+		return;
+
+	if (grid == NULL)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
