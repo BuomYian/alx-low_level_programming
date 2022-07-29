b505363099e6d10c1f6e41705a073acd1f3203dc
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-07-29 04:24:31 -0700

    print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
-#include "main.h"
 #include <stdio.h>
-#define UNUSED(x) (void)(x)
+
 /**
  * main - main function
  * @argc: argumentc
@@ -9,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	UNUSED(argc);
 	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 }
