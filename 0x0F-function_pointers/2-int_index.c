0076f1561698ff41f1d746a92d26b4264ed0f9cb
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-03 11:41:18 -0700

    fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
-  *int_index - indexes of array to be search
-  *@array: array to search
-  *@size: size of array
-  *@cmp: function to do something with size
-  *Return: return int value
-  */
+ * int_index - indexes aray i think
+ * @array: array to go through
+ * @size: size of array
+ * @cmp: function to do something with size
+ * Return: int value
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int 1;
+	int i;
 
 	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
