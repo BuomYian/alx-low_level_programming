a9db884eadb392e6a4aac8f4879ac1c80f4f7da1
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-03 11:23:06 -0700

    iterates through array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+  *array_iterator - iterates through array
+  *@array: array to go through
+  *@size: size of array
+  *@action: action function to take
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		exit(98);
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,4 +5,7 @@ int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
 
+void array_iteration(int *array, size_t size, void (*action)(int));
+
+
 #endif
