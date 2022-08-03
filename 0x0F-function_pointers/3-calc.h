a43a774f63de9924092eddd2c5d972adfe9e9dc1
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-03 12:28:54 -0700

    fixed calculator

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,6 @@
+#ifndef PROTOTYPES
+#define PROTOTYPES
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
@@ -17,3 +20,4 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+#endif
