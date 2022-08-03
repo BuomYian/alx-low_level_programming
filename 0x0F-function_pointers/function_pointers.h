55d25d1b24f2d0f67654280745523ddfbdfd6a31
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-03 11:24:39 -0700

    fixed

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,7 +5,7 @@ int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
 
-void array_iteration(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 #endif
