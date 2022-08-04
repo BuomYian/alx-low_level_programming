1ab11c2697d95e1e80c55bfa0dd2d093c1153b7c
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-04 11:08:16 -0700

    sum of arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,12 +9,17 @@
   */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum;
+	unsigned int i = 0;
+	int sum = 0;
+
+	va_list ap;
+
+	va_start(ap, n);
 
 	if (n == 0)
-	{
 		return (0);
-	}
-	sum = sum_them_all(n, ...);
-	return sum;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
 }
