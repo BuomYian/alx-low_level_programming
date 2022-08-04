8c8e6ea1c9d75ec03ee616d5e14d4337c3e42a81
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-04 11:33:47 -0700

    fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,13 +1,14 @@
 #include <stdarg.h>
 #include <stdio.h>
-#include "variadic_functions.h"
 
 /**
-  *print_numbers - print numbers
-  *@separator: item to use to separate numbers
-  *@n: number of argument to sum
-  *Return: numbers
-  */
+ * print_numbers - print numbers
+ * @separator: item to use ot separate numbers
+ * @n: number of argument to sum
+ * Return: sum of all argument
+ */
+
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
@@ -17,11 +18,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (!separator)
 		return;
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d%s", va_arg(ap, int), i != (n - 1) ? separator : "");
 	}
 
+
 	va_end(ap);
 	putchar('\n');
+
 }
