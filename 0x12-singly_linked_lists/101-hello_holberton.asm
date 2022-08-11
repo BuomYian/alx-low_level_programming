4b1dadcb6776f6f4f6628db0d90c0031e2006379
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-11 13:32:51 -0700

    last commit

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,13 @@
+section     .text
+    global      main
+
+main:
+    mov     edx,len
+    mov     ecx,msg
+    mov     ebx,1
+    mov     eax,4
+    int     0x80
+
+section     .data
+msg     db  'Hello, Holberton',0xa
+	len     equ $ - msg
