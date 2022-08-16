d6490918042494f02f286d2249a2f463380e50d5
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-16 00:33:25 -0700

    fixed

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,18 @@
 #include "lists.h"
-
 /**
- * print_listint - prints all the elements of a linked list
- * @h: head of the list
- *
- * Return: the number of nodes
- */
+  *print_listint - prints integers in list
+  *@h: struct listint_t type
+  *Return: size_t value
+  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *cursor = h;
 	size_t count = 0;
 
-	while (cursor != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", cursor->n);
-		count += 1;
-		cursor = cursor->next;
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
 	}
 	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
