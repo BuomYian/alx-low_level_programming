0ca5f059320e57c4de927eed7a0ba851ff99cb45
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-16 00:09:43 -0700

    fixed

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
-  *print_listint - prints all the elements of a linked lsit
-  *@h: head of the list
-  *
-  *Return: the number of nodes
-  */
-
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
+ *
+ * Return: the number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *cursor = h;
