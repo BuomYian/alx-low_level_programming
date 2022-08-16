c9f6be498f7836c98400e6d15b816556913db5cf
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-16 05:01:45 -0700

    fixed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -18,7 +18,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	}
 	else
 		return (NULL);
-	if (*heaad != NULL)
+	if (*head != NULL)
 		new_node->next = *head;
 	*head = new_node;
 	return (new_node);
