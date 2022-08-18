c1d184db78a1971ac0bd410a741ddcbca43219c1
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-18 11:45:02 -0700

    clear index to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "2-get_bit.c"
+/**
+  *clear_bit - clears index to 0
+  *@n: integer to pass
+  *@index: index to go to
+  *Return: returns integer val
+  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+	(*n) &= ~(1 << index);
+
+	if (get_bit((*n), index) == 0)
+		return (1);
+	return (-1);
+}
