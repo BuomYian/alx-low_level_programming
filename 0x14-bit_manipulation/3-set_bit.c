26a3bd325a97305ffc026ca5f7bc1bb512205db4
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-08-18 11:25:35 -0700

    fixed

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "2-get_bit.c"
+
 /**
   *set_bit - sets a bit
   *@n: integer to grap
@@ -8,7 +9,7 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 32)
-		reurn (-1);
+		return (-1);
 	(*n) |= 1 << index;
 	if (get_bit((*n), index) == 1)
 		return (1);
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 
 
+
 #endif
