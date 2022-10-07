65bc2467cba8f0512cca108d63f2a28808544128
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-10-06 23:25:11 -0700

    fixed

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
-#!/bin/bash 
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c 
+#!/bin/bash
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
